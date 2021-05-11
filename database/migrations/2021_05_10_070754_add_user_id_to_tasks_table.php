ef7594d62548dbbbb1c302f3797d7fde12d086cd
k_okada <knrly.001@gmail.com>
2021-05-11 00:56:45 +0000

    add foreign key constraint

---
+++ database/migrations/2021_05_10_070754_add_user_id_to_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //$table->bigIncrements('id');
+            //$table->unsignedBigInteger('user_id');
+            //$table->string('content');
+            
+            //外部キー制約
+            $table->string('tasklist_user_id_foreign');
+            //$table->foreign(外部キーを設定するカラム名)->references(参照先のカラム名)->on(参照先のテーブル名);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeign('tasks_user_id_foreign');
+        });
+    }
+}
