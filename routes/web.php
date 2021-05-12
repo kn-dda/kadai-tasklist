25b974259511938fa628630d06b90d7164a61981
k_okada <knrly.001@gmail.com>
2021-05-12 06:13:54 +0000

    delete spaces

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,20 @@ Route::get('/', 'TasksController@index');
 //認証付きのルーティング
 Route::group(['middleware' => ['auth']], function () {
     // 中略
-    Route::resource('tasks','TasksController', ['only' => ['index','show']]);
-    Route::resource('tasks','TasksController', ['only' => ['edit','update']]);
+    Route::resource('tasks','TasksController',['only' => ['index','show']]);
+    Route::resource('tasks','TasksController',['only' => ['edit','update']]);
     Route::resource('tasks','TasksController',['only'=> ['store','create']]);
-    Route::resource('tasks', 'TasksController', ['only' => ['store', 'destroy']]);
+    Route::resource('tasks','TasksController',['only' => ['store', 'destroy']]);
 });
 
 
 //Route::resource('tasks', 'TasksController');
 
 // 認証
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login')->name('login.post');
-Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+Route::get('login','Auth\LoginController@showLoginForm')->name('login');
+Route::post('login','Auth\LoginController@login')->name('login.post');
+Route::get('logout','Auth\LoginController@logout')->name('logout.get');
 
 // ユーザ登録
-Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
-Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup','Auth\RegisterController@register')->name('signup.post');
